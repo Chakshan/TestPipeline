baa9ad93ffd65bdce0017d4ddb1f92575a98231e
Chakshan <chakshan.kothakota@gmail.com>
2025-05-10 22:42:38 -0700

    actual refactor

---
+++ source/script/script.js
@@ -0,0 +1,57 @@
+/**
+ * Adds two numbers together.
+ * @param {number} a - First number
+ * @param {number} b - Second number
+ * @returns {number} Sum of a and b
+ */
+function add(a, b) {
+  return a + b;
+}
+
+/**
+ * Subtracts two numbers together.
+ * @param {number} a - First number
+ * @param {number} b - Second number
+ * @returns {number} Sum of a and b
+ */
+function subtract(a, b) {
+  return a + b;
+}
+
+/**
+ * Multiplies two number together
+ * @param {number} a - first number
+ * @param {number} b - second number
+ * @returns {number} product of a and b
+ */
+function multiply(a, b) {
+  return a * b;
+}
+
+/**
+ * Divides two number together
+ * @param {number} a - first number
+ * @param {number} b - second number
+ * @returns {number} quotient of a divided by b
+ */
+function divide(a, b) {
+  return a / b;
+}
+
+/**
+ * Does nothing
+ * @param {number} a - input
+ * @returns {number} - nothing new
+ */
+function yeye(a) {
+  return (a * 2) / 2 + 3 - 3;
+}
+
+/**
+ * Does nothing
+ * @param {number} a - input
+ * @returns {number} - nothing new
+ */
+function a(a) {
+  return (a * 2) / 2 + 3 - 3;
+}
